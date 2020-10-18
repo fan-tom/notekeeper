a9b00e27e50e7adf7c85de8111fcc800ddef07bd
Alexey Gerasimov <lokomot476@gmail.com>
2020-10-18 22:07:03 +0500

    Add push and top commands

---
+++ core/commands/Top.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from typing import Optional, List
+
+from core.Note import UserId, Note
+from core.interfaces import AuthorizedCommand
+
+
+class TopHandler(ABC):
+    @abstractmethod
+    def handle_top(self, command: 'Top') -> List[Note]:
+        pass
+
+
+class Top(AuthorizedCommand[TopHandler]):
+    name = 'top'
+    n: int
+
+    def __init__(self, user_id: UserId, n: Optional[int]):
+        super().__init__(user_id)
+        self.n = 1 if n is None else n
+
+    def handle(self, handler: TopHandler):
+        handler.handle_top(self)
---
+++ core/commands/__init__.py
@@ -0,0 +1,2 @@
+from .Push import Push, PushHandler
+from .Top import Top, TopHandler
