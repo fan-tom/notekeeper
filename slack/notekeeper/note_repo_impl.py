e7dc122b840816afd4865db36039cb2eb9360530
Alexey Gerasimov <lokomot476@gmail.com>
2020-10-20 22:16:59 +0500

    Implement note repo using note model

---
+++ slack/notekeeper/note_repo_impl.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from core.interfaces import NoteRepo
+from core.note import UserId, Note
+from notekeeper.models import NoteModel
+
+
+class NoteRepoImpl(NoteRepo):
+    def create(self, user_id: UserId, text: str) -> Note:
+        note_obj = NoteModel.objects.create(user_id=user_id, text=text)
+        return note_obj.to_note()
+
+    def get_last(self, user_id: UserId, n: int) -> List[Note]:
+        return list(map(lambda note: note.to_note(),
+                        NoteModel.objects
+                        .filter(user_id=user_id)
+                        .order_by('-created_at')[:n]
+                        )
+                    )
