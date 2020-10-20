99b0e106ff31ab823504033386b2a112c0d36869
Alexey Gerasimov <lokomot476@gmail.com>
2020-10-20 22:09:30 +0500

    Init django

---
+++ slack/slackbots/urls.py
@@ -0,0 +1,30 @@
+"""slackbots URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# from django.contrib import admin
+from django.urls import path, include
+
+from core.commands import Top, Push
+from core.processors import NoteKeeper
+from common.Router import Router
+from common.views import SlackHook
+from notekeeper.note_repo_impl import NoteRepoImpl
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    # TODO: probably, it is wrong place to configure router and command processor
+    path('slackhook/', SlackHook.as_view(router=Router(dict(notekeeper=NoteKeeper(NoteRepoImpl())), dict(push=Push, top=Top)))),
+    path('notekeeper/', include('notekeeper.urls'))
+]
