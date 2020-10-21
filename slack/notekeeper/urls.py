b56e24fc13a89c0d358891b77f7b99078491ca0f
Alexey Gerasimov <lokomot476@gmail.com>
2020-10-21 20:06:16 +0500

    Weaken url regex, add name to urls, change notes number url

--- slack/notekeeper/urls.py
+++ slack/notekeeper/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, re_path
 from notekeeper.admin_view import get_notes_number_per_period, get_top_used_words_per_period
 
 urlpatterns = [
-    re_path(r'stats/notes-per-period/(?P<user_id>U\d{11})?$', get_notes_number_per_period),
-    path('stats/top-used-words', get_top_used_words_per_period),
+    re_path(r'stats/notes-number/(?P<user_id>\w+)?$', get_notes_number_per_period, name='notes_number'),
+    path('stats/top-used-words', get_top_used_words_per_period, name='top_used_words'),
 ]
