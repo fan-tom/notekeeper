da9a4de137f45bb9e7a5268a7f88687bcd69d6fd
Alexey Gerasimov <lokomot476@gmail.com>
2020-10-21 19:58:49 +0500

    Weaken hook parser

--- slack/common/serializers/hook.py
+++ slack/common/serializers/hook.py
@@ -12,13 +12,13 @@ class TimestampField(serializers.DateTimeField):
 class HookSerializer(serializers.Serializer):
     token = serializers.CharField()  # snrQzbevYyXpBp8LA2q6hnuU
     team_id = serializers.RegexField(r'T\d+')  # T0001
-    team_domain = serializers.CharField()  # example
+    team_domain = serializers.CharField(required=False)  # example
 
     channel_id = serializers.RegexField(r'C\d+')  # C2147483705
     channel_name = serializers.CharField()  # test
 
     timestamp = TimestampField()  # 1355517523.000005
-    user_id = serializers.RegexField(r'U\d+')  # U2147483697
+    user_id = serializers.RegexField(r'U\w+')  # U2147483697
     user_name = serializers.CharField()  # Steve
 
     text = serializers.CharField()  # googlebot: What is the air-speed velocity of an unladen swallow?
